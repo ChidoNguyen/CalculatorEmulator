a163b171ea3baa0d5adc0e39cb34410af248b031
Chido Nguyen <chidonguyen@hotmail.com>
2019-05-28 13:58:45 -0700

    StartingPoint

---
+++ CalcEmul/CalcEmul/Source.cpp
@@ -0,0 +1,23 @@
+/*
+Chido Nguyen
+Code Challenge Plexsys: Calculator Emulator for * and + handling w/o using shunting algo
+*/
+
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+
+	string userProblem;
+	cout << "Please provide a math problem to solve with only + and * arthmitics" << endl;
+	cin >> userProblem;
+	cout << userProblem;
+
+
+	return 0;
+}
